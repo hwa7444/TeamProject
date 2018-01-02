53bcaf0128de9ee9b9dc59a809e6cd0864faa448
Byungwoo <kbw0519@naver.com>
2018-01-02 09:43:01 +0900

    병우 연동완료

--- app/src/main/java/codefactory/learfletproject/MainActivity.java
+++ app/src/main/java/codefactory/learfletproject/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //하하
+        //병우연동완료
     }
 }
