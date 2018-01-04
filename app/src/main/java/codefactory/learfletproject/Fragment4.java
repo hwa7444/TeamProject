6e59ba4a6f58d3d89cbe59346761a3a09f2bb261
윤지호 <jhyoon1128@gmail.com>
2018-01-04 21:06:25 +0900

    윤지호 죠스타

--- app/src/main/java/codefactory/learfletproject/Fragment4.java
+++ app/src/main/java/codefactory/learfletproject/Fragment4.java
@@ -3,22 +3,30 @@ package codefactory.learfletproject;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+
 
 /**
  * Created by jhyoo on 2018-01-04.
  */
-
 public class Fragment4 extends Fragment {
+    ScrollView scrollView;
+
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment4, container, false);
+
 
 
         return rootView;
 
     }
 }
+
+
