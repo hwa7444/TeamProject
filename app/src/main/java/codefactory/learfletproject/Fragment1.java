eff7513b4b5ccd717b836f516f0ba17e31012381
윤지호 <jhyoon1128@gmail.com>
2018-01-04 12:51:46 +0900

    윤지호 죠스타

---
+++ app/src/main/java/codefactory/learfletproject/Fragment1.java
@@ -0,0 +1,24 @@
+package codefactory.learfletproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jhyoo on 2018-01-04.
+ */
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+
+        return rootView;
+
+    }
+}
--- app/src/main/java/codefactory/learfletproject/mainscreen.java
+++ app/src/main/java/codefactory/learfletproject/mainscreen.java
@@ -2,12 +2,72 @@ package codefactory.learfletproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class mainscreen extends AppCompatActivity {
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+    Fragment4 fragment4;
+    Fragment5 fragment5;
 
+    ImageButton imgbtn_num1 = null;
+    ImageButton imgbtn_num2 = null;
+    ImageButton imgbtn_num3 = null;
+    ImageButton imgbtn_num4 = null;
+    ImageButton imgbtn_num5 = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+        fragment5 = new Fragment5();
+
+
+        imgbtn_num1 = (ImageButton)findViewById(R.id.imgbtn_num1);
+        imgbtn_num1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment1).commit();
+            }
+        });
+
+        imgbtn_num2 = (ImageButton)findViewById(R.id.imgbtn_num2);
+        imgbtn_num2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment2).commit();
+            }
+        });
+
+        imgbtn_num3 = (ImageButton)findViewById(R.id.imgbtn_num3);
+        imgbtn_num3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment3).commit();
+            }
+        });
+
+        imgbtn_num4 = (ImageButton)findViewById(R.id.imgbtn_num4);
+        imgbtn_num4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment4).commit();
+            }
+        });
+
+        imgbtn_num5 = (ImageButton)findViewById(R.id.imgbtn_num5);
+        imgbtn_num5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment5).commit();
+            }
+        });
     }
 }
+
